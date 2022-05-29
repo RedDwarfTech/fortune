d78cf6a84006da1ede280f4b20df43a99f9f3297
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-05-29 23:51:16 +0800

    feat: add rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::{Build, Rocket, routes};
+
+mod common;
+
+use common::health_controller;
+
+#[launch]
+async fn rocket() -> _ {
+    build_rocket()
 }
+
+fn build_rocket() -> Rocket<Build> {
+    rocket::build()
+        .mount("/actuator", routes![
+            health_controller::health,
+            health_controller::liveness
+        ])
+}
+
