b6f694cb66a0946ceccc80442c1ab1f8ea5293dc
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-06-01 23:40:39 +0800

    refactor: tweak the database url config

--- src/service/contents/contents_service.rs
+++ src/service/contents/contents_service.rs
@@ -3,10 +3,10 @@ use rust_wheel::config::db::config;
 use crate::model::diesel::fortune::fortune_models::FortuneContent;
 use crate::diesel::ExpressionMethods;
 
-pub fn content_tree_query<T>(contents_type: i32) -> Vec<FortuneContent> {
+pub fn content_tree_query<T>(filter_content_type: i32) -> Vec<FortuneContent> {
     use crate::model::diesel::fortune::fortune_schema::fortune_contents::dsl::*;
-    let connection = config::establish_connection();
-    let predicate = crate::model::diesel::fortune::fortune_schema::fortune_contents::contents_type.eq(contents_type);
+    let connection = config::connection("FORTUNE_DATABASE_URL".to_string());
+    let predicate = contents_type.eq(filter_content_type);
     let contents = fortune_contents.filter(&predicate)
         .load::<FortuneContent>(&connection)
         .expect("Error fortune contents resource");
