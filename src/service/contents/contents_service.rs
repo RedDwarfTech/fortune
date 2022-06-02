695f54f2babb10ee9dcae662ee245d689d5041e8
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-06-02 14:40:37 +0800

    refactor: return the object

--- src/service/contents/contents_service.rs
+++ src/service/contents/contents_service.rs
@@ -3,9 +3,10 @@ use rocket::serde::json::serde_json;
 use rust_wheel::config::db::config;
 use crate::model::diesel::fortune::fortune_models::FortuneContent;
 use crate::diesel::ExpressionMethods;
+use crate::model::response::contents::fortune_contents_response::FortuneContentResponse;
 use crate::service::contents::convert_to_tree::convert_to_tree;
 
-pub fn content_tree_query<T>(filter_content_type: i32) -> String {
+pub fn content_tree_query<T>(filter_content_type: i32) -> Vec<FortuneContentResponse> {
     use crate::model::diesel::fortune::fortune_schema::fortune_contents::dsl::*;
     let connection = config::connection("FORTUNE_DATABASE_URL".to_string());
     let predicate = contents_type.eq(filter_content_type);
@@ -15,14 +16,14 @@ pub fn content_tree_query<T>(filter_content_type: i32) -> String {
     return convert_to_tree_impl(&contents);
 }
 
-pub fn convert_to_tree_impl(contents: &Vec<FortuneContent>) -> String{
-    let mut root_element:Vec<_> = contents.iter()
+pub fn convert_to_tree_impl(contents: &Vec<FortuneContent>) -> Vec<FortuneContentResponse>{
+    let root_element:Vec<_> = contents.iter()
         .filter(|content|content.parent_id==0)
         .collect();
-    let mut sub_element:Vec<_> =  contents.iter()
+    let sub_element:Vec<_> =  contents.iter()
         .filter(|content|content.parent_id!=0)
         .collect();
     let result = convert_to_tree(&root_element, &sub_element);
-    return serde_json::to_string_pretty(&result).unwrap();
+    return result;
 }
 
