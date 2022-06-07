c5d22517491c2478ab87ee42574b46a95110245d
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-06-07 13:07:28 +0000

    add okapi dependencies

--- src/model/request/bill/bill_request.rs
+++ src/model/request/bill/bill_request.rs
@@ -4,5 +4,7 @@ use rocket::serde::Serialize;
 #[derive(Debug, PartialEq, Eq, Deserialize, Serialize)]
 #[allow(non_snake_case)]
 pub struct BillRequest {
-
+    pub billBookId: i64,
+    pub contentsId: i64,
+    pub amount: i64
 }
